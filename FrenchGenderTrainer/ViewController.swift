52e4a7d132b0a10578516870367d3aade5ef3958
Kathrin <gypsy_kat@yahoo.com>
2019-03-07 07:57:24 -0500

    Initial Commit

---
+++ FrenchGenderTrainer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FrenchGenderTrainer
+//
+//  Created by KATHRIN AYER on 3/7/19.
+//  Copyright © 2019 happierplaces. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
